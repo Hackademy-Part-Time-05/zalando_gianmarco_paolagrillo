07d6f97901cb6cc9faf9e6c04554569c2ad4edeb
Giammy88 <conte.gianmarco@yahoo.it>
2023-05-25 10:48:06 +0200

    Aggiunte installazioni varie

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,42 @@
+<div class="container py-4">
+  <form action="{{route('store')}}" method="POST">
+    @csrf
+    <div class="mb-3">
+      <label class="form-label">Id</label>
+      <input class="form-control" type="text" name="product_id" readonly value="{{$item->id}}"
+        placeholder="Identificativo" />
+    </div>
+    <div class="mb-3">
+      <label class="form-label">Nome Prodotto</label>
+      <input class="form-control" type="text" name="product_name" readonly value="{{$item->title}}"
+        placeholder="Nome prodotto" />
+    </div>
+    <div class="mb-3">
+      <label class="form-label">Prezzo</label>
+      <input class="form-control" type="text" name="product_price" value="{{$item->price}}" readonly
+        placeholder="Prezzo" />
+    </div>
+    <hr>
+    <div class="mb-3">
+      <label class="form-label">Cognome</label>
+      <input class="form-control" type="text" name="surname" placeholder="Cognome" value="" />
+    </div>
+    <div class="mb-3">
+      <label class="form-label">Nome</label>
+      <input class="form-control" type="text" name="name" placeholder="Nome" value="" />
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">Email</label>
+      <input class="form-control" type="text" name="email" placeholder="Email" value="" />
+    </div>
+
+
+    <!--  -->
+    <div class="d-grid">
+      <button class="btn btn-primary btn-lg" type="submit">Invia</button>
+    </div>
+
+  </form>
+
+</div>
